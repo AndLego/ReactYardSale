258193c4f9ed28a3059194fa6e09c64069c4a49f
AndLego <galeon.gb@gmail.com>
2022-04-03 16:57:00 -0500

    creada base de react, webpack, y sass

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <h1>Holi, esto es una simple prueba de react</h1>
+    );
+}
+
+export default App;
